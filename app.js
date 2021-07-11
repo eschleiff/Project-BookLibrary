1540670fc840f88db6b7d3cf4926fff4c3d1570b
Eric Schleiff <ericschleiff@gmail.com>
2021-07-11 11:24:41 -0500

    added routes and controllers

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 var app = express();
+
+// routes setup
+app.use('/catalog', catalogRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
